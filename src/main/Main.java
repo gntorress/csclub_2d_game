baab3da5d28b4f137d088debdcb7d8918b3944cb
apetbrz <apetbrz@gmail.com>
2024-02-22 17:18:51 -0500

    Polish for Coding Meet 2.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     //MAP_SELECTION: the chosen map to load
     //WILL REMOVE AFTER MAP SELECTION IS IMPLEMENTED
-    public static final String MAP_SELECTION = "beach";
+    public static final String MAP_SELECTION = "default";
 
     //TARGET_FPS: the fps to try to render at
     public static final int TARGET_FPS = 60;
